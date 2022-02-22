553622acacaff2dab1ebd727178c510192dfbf83
Kezi Rose <keziahflorence@gmail.com>
2022-02-22 17:44:48 +0100

    new database started

---
+++ model/init_db.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS mvp_users;
+CREATE TABLE mvp_users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    pronouns VARCHAR(50),
+    email VARCHAR(100),
+    city VARCHAR(100),
+    pricepoint VARCHAR(100),
+    style_one VARCHAR(100)
+);
+INSERT INTO mvp_users (first_name, last_name, pronouns, email, city, pricepoint, style_one)
+    VALUES ('Kezi', 'Rose', 'She/Her', 'email@email.com', NULL, NULL, NULL);
+
+
+DROP TABLE IF EXISTS mvp_artists;
+CREATE TABLE mvp_artists (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    pronouns VARCHAR(50),
+    email VARCHAR(100),
+    instagram VARCHAR(100),
+    studio VARCHAR(100),
+    city VARCHAR(100),
+    pricepoint VARCHAR(100),
+    style_one VARCHAR(100)
+);
+INSERT INTO mvp_artists (first_name, last_name, pronouns, email, instagram, studio, city, pricepoint, style_one)
+    VALUES ('Indigo', 'Blue', 'She/Her', 'email@email.com', '@indi_blue_tattoos', 'Independent Artist', 'London', '£150', 'Black & Grey');
+    
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('welcome to the mvp');
 });
+
+// router.get("/users", (req, res) => {
+//   // Send back the full list of items
+//   db("SELECT * FROM mvp_users;")
+//     .then(results => {
+//       res.send(results.data);
+//     })
+//     .catch(err => res.status(500).send(err));
+// });
 
 module.exports = router;
